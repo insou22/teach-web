9bc7c0f752bcff6814bb8204ddc1e1e57530f5fe
insou22 <zac.kologlu@gmail.com>
2020-06-18 12:46:07 +1000

    update smb.c

--- public/classdata/thu11b/tutorials/3/six_middle_bits.c
+++ public/classdata/thu11b/tutorials/3/six_middle_bits.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 #include <stdint.h>
 
-uint32_t six_middle_bits(uint32_t number);
+#define SIX_MIDDLE_SHIFT 13
+#define SIX_BIT_MASK 0x3F;
+
+uint8_t six_middle_bits(uint32_t number);
+
 
 int main(void) {
+    // prints 63 as 6 middle bits are all 1's
+    printf("%d\n", six_middle_bits(0b1100101011111110000110100000));
+}
+
+//                 <----->
+// 0000 0000 0000 0111 1110 0000 0000 0000
+uint8_t six_middle_bits(uint32_t number) {
+// 0000 0000 0000 0000 0000 0000 0011 1111
+    uint32_t shifted = number >> SIX_MIDDLE_SHIFT;
     
-}
+    uint32_t number_masked = shifted & SIX_BIT_MASK;
 
-#define MY_MASK 0x0007D000
-// 00000000 00000111 11100000 00000000
-uint32_t six_middle_bits(uint32_t number) {
-    // note - this will leave us with the six middle bits left in the middle.
-    return number & MY_MASK;
-}
-
-#define MY_MASK_2 0x3F
-// 00000000 00000000 00000000 00111111
-uint32_t six_middle_bits(uint32_t number) {
-    // note - this will leave us with the six middle bits at the end now.
-    return (number >> 13) & MY_MASK_2;
+    return number_masked;
 }
